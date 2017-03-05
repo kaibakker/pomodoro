2fb50e75e5cd69203b9b07c52809ff2f3d345196
Kai Bakker <kaibakker@gmail.com>
2017-03-05 19:00:25 +0100

    basic version + cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -2,82 +2,28 @@ import React from 'react';
 
 
 
-var Player = require('./Player')
 var TaskList = require('./TaskList')
 
 import './../App.css';
 
+import randomColor from 'randomcolor'
+
+
+var tasks = JSON.parse(localStorage.getItem('tasks')) || [
+  {label: 'Not active', color: 'red', logs: [{ startedAt: Date.now() }]},
+  {label: 'Lunch', color: randomColor({luminosity: 'light'}), logs: []},
+  {label: 'Break', color: randomColor({luminosity: 'light'}), logs: []},
+];
 
 
 var App = React.createClass({
   getInitialState() {
     return ({
-      startedAt: null,
-      logs: []
+      tasks: tasks,
     })
-  },
-  start() {
-    this.setState({ startedAt: Date.now() })
-  },
-  stop() {
-    this.setState({
-      startedAt: null,
-    })
-  },
-  finish() {
-    var logs = this.state.logs.slice()
-
-    logs.unshift({
-      startedAt: this.state.startedAt,
-      endedAt: Date.now(),
-      name: this.state.name,
-    })
-
-    this.setState({
-      logs: logs,
-      startedAt: null,
-      name: ''
-    })
-  },
-  completeCallback() {
-    this.finish()
-  },
-  handleNameChange(e) {
-    this.setState({ name: e.target.value })
   },
   render() {
-    return (<TaskList />)
-    return (
-      <div className="container">
-        <div className="App-header">
-          <h2>Pomodoro timer</h2>
-        </div>
-
-        <div>
-          <Player startedAt={ this.state.startedAt } completeCallback={ this.completeCallback }/>
-        </div>
-
-        <div className="input-group">
-          <input type="text" className="form-control" value={ this.state.name } onChange={ this.handleNameChange } placeholder='What are you doing?' />
-          <span className="input-group-btn">
-            { this.state.startedAt &&
-              <button className="btn btn-default" type="button" onClick={ this.finish }>finish Task</button>
-            }
-            { !this.state.startedAt &&
-              <button className="btn btn-default" type="button" onClick={ this.start }>Start task</button>
-            }
-          </span>
-        </div>
-
-
-
-        <ul>
-          { this.state.logs.map(function(log) {
-            return <li>{log.name} { (log.endedAt - log.startedAt)/1000 }</li>
-          })}
-        </ul>
-      </div>
-    );
+    return (<TaskList tasks={tasks}/>)
   }
 })
 
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,48 +1,102 @@
 import React, {Component} from 'react';
-import {render} from 'react-dom';
+// import {render} from 'react-dom';
 import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
+
+import Moment from 'react-moment';
 
 import randomColor from 'randomcolor'
 
-const TaskElement = SortableElement(({item}) => <li style={{backgroundColor: item.color}}>{item.label}</li>);
 
-const TaskContainer = SortableContainer(({items}) => {
-    return (
-        <ul>
-            {items.map((item, index) =>
-                <TaskElement key={`item-${index}`} index={index} item={item} />
-            )}
-        </ul>
-    );
-});
-var tasks = [
-  {label: 'Not active', color: 'red'},
-  {label: 'Lunch', color: randomColor()},
-  // {label: 'Upcoming:', color: '#fff'},
-  {label: 'Break', color: randomColor()},
-  {label: 'Working on UX design			', color: randomColor()},
-  {label: 'Finish this review!', color: randomColor()},
-  // {label: 'Common:', color: '#fff'},
-  {label: 'Sinterklaas', color: randomColor()},
-  // {label: 'Done:', color: '#fff'},
-  {label: 'Get #1 google ranking', color: randomColor()},
-  {label: 'Update to-do list article', color: randomColor()}
-];
+
+class Counter extends Component {
+  render() {
+    var counter = this.props.logs.map(function({startedAt, endedAt}) {
+      if(endedAt) {
+        return endedAt - startedAt
+      } else {
+        return (- startedAt)
+      }
+    }).reduce(function(a, b) { return a + b; }, 0)
+
+    if(counter < 0) {
+      return (
+        <span className='pull-xs-right'>
+          <Moment ago={true} fromNow>{ ((new Date(-counter)).toUTCString() ) }</Moment>
+        </span>
+      )
+    } else {
+      return (
+        <span className='pull-xs-right'>
+          <Moment ago={true} from={new Date()}>{ ((new Date(Date.now() - counter)).toUTCString() ) }</Moment>
+        </span>
+      )
+    }
+  }
+}
+
+const TaskElement = SortableElement(({item}) =>
+  <li style={{backgroundColor: item.color}}>
+    {item.label}
+    <Counter logs={item.logs} />
+
+  </li>
+);
+
+const TaskContainer = SortableContainer(({items}) =>
+  <ul>
+      {items.map((item, index) =>
+          <TaskElement key={`item-${index}`} index={index} item={item} />
+      )}
+  </ul>
+);
+
 class TaskList extends Component {
-
     state = {
-        items: tasks
-
+      items: this.props.tasks
+    }
+    shouldCancelStart = (e) => {
+      if (['input', 'textarea', 'select', 'option', 'button'].indexOf(e.target.tagName.toLowerCase()) !== -1) {
+					return true; // Return true to cancel sorting
+      }
     }
     onSortEnd = ({oldIndex, newIndex}) => {
-        this.setState({
-            items: arrayMove(this.state.items, oldIndex, newIndex)
-        });
+      var items = this.state.items
+
+      if(oldIndex === 0) {
+        items[0].logs[0].endedAt = Date.now()
+        items[1].logs.unshift({ startedAt: Date.now() })
+      } else if(newIndex === 0) {
+        items[0].logs[0].endedAt = Date.now()
+        items[oldIndex].logs.unshift({ startedAt: Date.now() })
+      }
+
+      var newItems = arrayMove(items, oldIndex, newIndex)
+
+      this.setState({
+          items: newItems
+      });
+
+      this.saveTasksToLocalStorage(newItems)
     };
+    createTask = (event) => {
+      var items = this.state.items
+      items.push({ label: event.target.value, color: randomColor({ luminosity: 'light' }), logs: []})
+      this.setState({
+        items: items
+      });
+      this.saveTasksToLocalStorage(items)
+    };
+    saveTasksToLocalStorage = (items) => {
+      localStorage.setItem("tasks", JSON.stringify(items))
+    }
     render() {
-        return (
-            <TaskContainer items={this.state.items} onSortEnd={this.onSortEnd} />
-        )
+      return (
+        <div>
+          <TaskContainer items={this.state.items} onSortEnd={this.onSortEnd} shouldCancelStart={this.shouldCancelStart} />
+
+          <input type='text' onBlur={this.createTask} />
+        </div>
+      )
     }
 }
 
