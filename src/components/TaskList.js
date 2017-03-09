50bacfda5adea980d4f76e7fd6d5591bcaa1cc09
Kai Bakker <kaibakker@gmail.com>
2017-03-10 00:03:23 +0100

    created timeline

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -6,55 +6,23 @@ import Moment from 'react-moment';
 
 import randomColor from 'randomcolor'
 
+var tasks = [
+  {label: 'Not active', color: 'red', logs: [{ startedAt: new Date().setHours(12), endedAt: new Date().setHours(14) }], status: 'created' },
+  {label: 'Lunch', color: randomColor({luminosity: 'light'}), logs: [{ startedAt: new Date().setHours(15), endedAt: new Date().setHours(16) }], status: 'created' },
+  {label: 'Break', color: randomColor({luminosity: 'light'}), logs: [{ startedAt: new Date().setHours(17), endedAt: new Date().setHours(19) }], status: 'created' },
+];
 
-
-class Counter extends Component {
+class Task extends Component {
   render() {
-    var counter = this.props.logs.map(function({startedAt, endedAt}) {
-      if(endedAt) {
-        return endedAt - startedAt
-      } else {
-        return (- startedAt)
-      }
-    }).reduce(function(a, b) { return a + b; }, 0)
-
-    if(counter < 0) {
-      return (
-        <span className='pull-xs-right'>
-          <Moment ago={true} fromNow>{ ((new Date(-counter)).toUTCString() ) }</Moment>
-        </span>
-      )
-    } else {
-      return (
-        <span className='pull-xs-right'>
-          <Moment ago={true} from={new Date()}>{ ((new Date(Date.now() - counter)).toUTCString() ) }</Moment>
-        </span>
-      )
-    }
+    var date = new Date()
+    date.setTime(this.props.task.logs[0].endedAt)
+    return (
+      <div style={{ height: (this.props.task.logs[0].endedAt - this.props.task.logs[0].startedAt)/(90*1000)}}>
+        { date.toTimeString() }
+      </div>
+    )
   }
 }
-
-const TaskElement = SortableElement(({item}) =>
-  <li style={{backgroundColor: item.color}}>
-    {item.label}
-    <Counter logs={item.logs} />
-    <a onClick={this}>archive</a>
-  </li>
-);
-
-const TaskContainer = SortableContainer(({items}) =>
-  <ul>
-    {items.map((item, index) =>
-        <TaskElement key={`item-${index}`} index={index} item={item}  />
-    )}
-  </ul>
-);
-
-var tasks = JSON.parse(localStorage.getItem('tasks')) || [
-  {label: 'Not active', color: 'red', logs: [{ startedAt: Date.now() }], status: 'created' },
-  {label: 'Lunch', color: randomColor({luminosity: 'light'}), logs: [], status: 'created' },
-  {label: 'Break', color: randomColor({luminosity: 'light'}), logs: [], status: 'created' },
-];
 
 
 class TaskList extends Component {
@@ -66,25 +34,6 @@ class TaskList extends Component {
 				return true; // Return true to cancel sorting
       }
     }
-    onSortEnd = ({oldIndex, newIndex}) => {
-      var items = this.state.items
-
-      if(oldIndex === 0) {
-        items[0].logs[0].endedAt = Date.now()
-        items[1].logs.unshift({ startedAt: Date.now() })
-      } else if(newIndex === 0) {
-        items[0].logs[0].endedAt = Date.now()
-        items[oldIndex].logs.unshift({ startedAt: Date.now() })
-      }
-
-      var newItems = arrayMove(items, oldIndex, newIndex)
-
-      this.setState({
-          items: newItems
-      });
-
-      this.saveTasksToLocalStorage(newItems)
-    };
     createTask = (event) => {
       var items = this.state.items
       items.push({ label: event.target.value, color: randomColor({ luminosity: 'light' }), logs: []})
@@ -98,7 +47,9 @@ class TaskList extends Component {
     render() {
       return (
         <div>
-          <TaskContainer items={this.state.items} onSortEnd={this.onSortEnd} shouldCancelStart={this.shouldCancelStart}/>
+          { this.state.items.map(function(task) {
+            return <Task task={task} />
+          }) }
 
           <input type='text' onBlur={this.createTask} />
         </div>
